a03b79dcc0556c59aea8fd9142545954b8fd6538
dennis dentrix <denkyusya@gmail.com>
2026-04-18 16:43:37 +0300

    sitemap update

--- apps/web/src/middleware.js
+++ apps/web/src/middleware.js
@@ -31,5 +31,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|public).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|public).*)'],
 };
